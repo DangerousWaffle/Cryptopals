0a6726c5527a85b302d727f7a9505feee76af150
DangerousWaffle <waffledangerous@gmail.com>
2017-07-17 18:33:36 +0100

    Separate conversion methods

--- Python/Set1/challenge1.py
+++ Python/Set1/challenge1.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 import base64
 
-def convertHexToBase64(hexString):
-	#Convert to byte array
+def convertHexToBytes(hexString):
 	byteA = bytearray.fromhex(hexString)
-	return base64.b64encode(byteA).decode("utf-8")
+	return byteA
 
+def convertBytesToBase64(byteArray):
+	return base64.b64encode(byteArray).decode("utf-8")
+	
+
+def convertHexToBase64(hexString):
+	return convertBytesToBase64(convertHexToBytes(hexString))
