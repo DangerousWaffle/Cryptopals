96fa9c541ed016e25359f181afebc5b908c577e1
DangerousWaffle <waffledangerous@gmail.com>
2017-07-17 23:13:42 +0100

    Added reverse encoding methods

--- Python/Set1/challenge1.py
+++ Python/Set1/challenge1.py
@@ -5,9 +5,19 @@ def convertHexToBytes(hexString):
 	byteA = bytearray.fromhex(hexString)
 	return byteA
 
+def convertBytesToHex(byteArray):
+	#Starting with the empty string, add each element in the byteArray
+	#02x pads the string when needed
+	return ''.join(format(x, '02x') for x in byteArray)
+
 def convertBytesToBase64(byteArray):
 	return base64.b64encode(byteArray).decode("utf-8")
 	
+def convertBase64ToBytes(b64String):
+	return base64.b64decode(b64String)
 
 def convertHexToBase64(hexString):
 	return convertBytesToBase64(convertHexToBytes(hexString))
+
+def convertBase64ToHex(b64String):
+	return convertBytesToHex(convertBase64ToBytes(b64String))
--- Python/Set1/testChallenge1.py
+++ Python/Set1/testChallenge1.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 import challenge1
 
+toDecode = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
 def testChallenge1():
-        toDecode = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-        expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
         decoded = challenge1.convertHexToBase64(toDecode)
         if (decoded == expected):
                 return True
         else:
                 return False
 
+def testChallenge1Rev():
+	encoded = challenge1.convertBase64ToHex(expected)
+	if (encoded == toDecode):
+		return True
+	else:
+		return False
 
 if __name__ == "__main__":
         if (testChallenge1()):
-                print("Program 1 success");
+                print("Challenge 1 success");
         else:
-                print("Failure");
+                print("Challenge 1 Failure");
 
+        if (testChallenge1Rev()):
+                print("Challenge 1 reversed success");
+        else:
+                print("Challenge 1 reversed Failure");
