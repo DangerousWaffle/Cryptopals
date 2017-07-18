19665c85b30148e4beec0c0c7a7fa258dfd58aa7
Dangerous Waffle <waffledangerous@gmail.com>
2017-07-18 23:36:36 +0100

    Fixed Chall2 issues

--- Python/Set1/challenge2.py
+++ Python/Set1/challenge2.py
@@ -3,6 +3,6 @@
 def performXOR(byteArray1,byteArray2):
 	result = bytearray()
 	for idx,byte in enumerate(byteArray1):
-		result.extend(byte ^ byteArray2[idx])
+		result.append(byte ^ byteArray2[idx])
 	return result
 	
