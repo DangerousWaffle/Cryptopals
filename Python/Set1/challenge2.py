9c47e38863204c3767b8803d5d321cca344b6876
Dangerous Waffle <waffledangerous@gmail.com>
2017-07-18 23:49:28 +0100

    Case of strings of different length in XOR

--- Python/Set1/challenge2.py
+++ Python/Set1/challenge2.py
@@ -2,7 +2,13 @@
 
 def performXOR(byteArray1,byteArray2):
 	result = bytearray()
-	for idx,byte in enumerate(byteArray1):
-		result.append(byte ^ byteArray2[idx])
+
+	if len(byteArray1) >= len(byteArray2):
+		for idx,byte in enumerate(byteArray1):
+			result.append(byte ^ byteArray2[idx%len(byteArray2)])
+	else:
+		for idx,byte in enumerate(byteArray2):
+			result.append(byte ^ byteArray1[idx%len(byteArray1)])
+
 	return result
 	
