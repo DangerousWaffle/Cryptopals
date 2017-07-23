a9f34a28296abe4192782b51662702539634e6e0
DangerousWaffle <waffledangerous@gmail.com>
2017-07-23 23:19:55 +0100

    Start of improvements to string scoring

--- Python/Set1/Modules/VigenereDecoder.py
+++ Python/Set1/Modules/VigenereDecoder.py
@@ -1,16 +1,45 @@
 #!/usr/bin/python3
 import XOR
 
-letterFrequencyLkup = "etaoinshrdlcumwfgypbvkjxqz"
+freqLkupDict = {
+	"a":8.167,
+	"b":1.492,
+	"c":2.782,
+	"d":4.253,
+	"e":12.702,
+	"f":2.228,
+	"g":2.015,
+	"h":6.094,
+	"i":6.966,
+	"j":0.153,
+	"k":0.772,
+	"l":4.025,
+	"m":2.406,
+	"n":6.749,
+	"o":7.507,
+	"p":1.929,
+	"q":0.095,
+	"r":5.987,
+	"s":6.327,
+	"t":9.056,
+	"u":2.758,
+	"v":0.978,
+	"w":2.360,
+	"x":0.150,
+	"y":1.974,
+	"z":0.074
+}
 
 def scoreString(input):
+	letterIndex = 97
 	score = 0
-	for letter in input.lower():
-		try:
-			index = letterFrequencyLkup.index(chr(letter))
-			score += 26 - index
-		except:
-			pass
+	stringLen = len(input)
+
+	while letterIndex < 123:
+		percent = input.count(letterIndex) * 100 / stringLen
+		score = score + abs(percent - freqLkupDict[chr(letterIndex)]) 
+		letterIndex = letterIndex + 1
+
 	return score
 
 def decodeString(input):
@@ -20,8 +49,9 @@ def decodeString(input):
 
 	while letterIndex <= 123:
 		currentString = XOR.performXOR(input, [letterIndex])
-		#print (currentString.decode("utf-8"))
 		currentScore = scoreString(currentString)
+		print(currentString)
+		print(currentScore)
 		if (currentScore > bestScore):
 			bestScore = currentScore
 			bestResult = currentString
